9e48bb65c965aeb5f4addf68f55e28bb387a5695
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-08-27 23:04:48 -0700

    created html file with template

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // console.log(profileDataArgs);
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const [name, github] = profileDataArgs;
@@ -23,5 +25,8 @@ const generatePage = (userName, githubName) => {
     `;
 };
 
-console.log(name, github);
-console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err=> {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
