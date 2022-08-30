ab1d90ef1ca35bed41ce1995e08d57ff4f4ac16e
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-08-29 23:11:14 -0700

    .map projects page template

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,39 @@
+//create the about section
+const generateAbout = aboutText => {
+  if(!aboutText) {
+    return '';
+  }
+
+  return `
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
+      </section>
+      `;
+};
+
+const generateProjects = projectsArr => {
+  const projectHtmlArr = projectsArr.map(({name, description, languages, link}) => {
+    return `
+    <section class="my-3" id="portfolio">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+      <div class="flex-row justify-space-between">
+        <!-- Leaving this empty as we'll dynamically insert a project HTML here -->
+      </div>
+    </section>
+  `;
+});
+
+  return `
+    <section class="my-3" id="portfolio">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+      <div class="flex-row justify-space-between">
+      ${projectHtmlArr.join('')}
+      </div>
+    </section>
+    `;
+};
+
 module.exports = (templateData) => {
     console.log(templateData);
     
@@ -22,6 +58,10 @@ const { projects, about, ...header } = templateData;
     <body>
       <h1>${templateData.name}</h1>
       <h2><a href="https://github.com/${templateData.github}">Github</a></h2>
+      <main class="container my-5">
+        ${generateAbout(about)}
+        ${generateProjects(projects)}
+      </main>
     </body>
     </html>
     `;
